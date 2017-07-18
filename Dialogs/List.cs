2b80e769982ae6c36d539d7c671b565471740390
davidifeguni <t-daifeg@microsoft.com>
2017-07-18 16:10:49 -0700

    Add files via upload

---
+++ Dialogs/List.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace LuisBot.Dialogs
+{
+    class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public Excel()
+        { 
+        }
+
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+        }
+
+        public void CreateNewFile()
+        {
+            this.wb = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            if (ws.Cells[i, j].Value2 != null)
+                return ws.Cells[i, j].Value2;
+            else
+                return "";
+        }
+
+    };
+}
---
+++ Dialogs/RootLuisDialog.cs
@@ -0,0 +1,53 @@
+﻿namespace LuisBot.Dialogs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.FormFlow;
+    using Microsoft.Bot.Builder.Luis;
+    using Microsoft.Bot.Builder.Luis.Models;
+    using Microsoft.Bot.Connector;
+    using Microsoft.Office.Interop.Excel;
+    using _Excel = Microsoft.Office.Interop.Excel;
+
+    [LuisModel("7593ec5a-3119-4e83-b9f5-2766444735e8", "732d85b83c644b7e8bff565529a19b84")]
+    [Serializable]
+    public class RootLuisDialog : LuisDialog<object>
+    {
+        private const string EntityItem = "Item";
+
+        private const string EntityAddToCart = "AddToCart";
+
+        
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"I'm sorry, I did not understand '{result.Query}'."); //
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("AddToCart")]
+        public async Task MyIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Ok, let me look for this item."); //
+            // Cortana takes picture of item.
+            // Sends picture to custom vision service through server
+            // Detects what kind of cereal it is and adds it to an excel or word document.
+            await context.PostAsync($"I will add this item to your shopping cart");
+            context.Wait(MessageReceived);
+        }
+    }
+}
+
+
+
+
+           
+        
